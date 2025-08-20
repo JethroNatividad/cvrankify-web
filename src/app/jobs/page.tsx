ce084a343115c9f4067a423eb0a3027cda81b9ef
JethroNatividad <natividad.jet@gmail.com>
2025-08-20 23:39:28 +0800

    feat: update Navbar to handle optional user prop and add PublicJobs component

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-import { Button } from "../_components/ui/button";
+import { HydrateClient } from "~/trpc/server";
 import PublicJobs from "./_components/public-jobs";
 
 export default async function Home() {
-  const session = await auth();
-
   return (
     <HydrateClient>
       <div className="container mx-auto">
