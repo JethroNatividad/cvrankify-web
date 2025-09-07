7d4df90db40079db1975dcdabfefb904c0558442
JethroNatividad <natividad.jet@gmail.com>
2025-09-08 05:31:12 +0800

    feat: simplify Home component to redirect to jobs page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import Link from "next/link";
-
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
-  // const session = await auth();
-
-  // if (session?.user) {
-  //   void api.post.getLatest.prefetch();
-  // }
-
-  return (
-    <HydrateClient>
-      <main>Home</main>
-    </HydrateClient>
-  );
+  redirect("/jobs");
 }
